ca37596f2c057f8144cf9dc5631c923fbc628740
milafrerichs <mila.frerichs@gmail.com>
2016-07-24 14:59:28 +0200

    Add authors to scope and sort theses by date correctly

--- web/js/causa/context/contextController.js
+++ web/js/causa/context/contextController.js
@@ -1,12 +1,33 @@
-angular.module('causa').controller('ContextController', ['thesis', 'article', 'vote', '$scope', '$http', function(thesis, article, vote, $scope, $http) {
+angular.module('causa').controller('ContextController', ['thesis', 'article', 'vote', 'author', '$scope', '$http', '$q', function(thesis, article, vote, author, $scope, $http, $q) {
+  var sortByDistance= function(results) {
+    var thesisId = "39926eec0868833d6729637c229419c5";
+    return results.sort(function(a,b) {
+      return $scope.distances[thesisId][a.id] - $scope.distances[thesisId][b.id];
+    });
+  };
+  var sortByArticleDate = function(results) {
+    return results.sort(function(a,b) {
+      return a.article.date - b.article.date;
+    });
+  };
   $scope.debates = [];
   $scope.theses = [];
   $scope.articles = [];
   $scope.votes = [];
+  $scope.authors = [];
+  $scope.authorsLookup = {};
+  $http.get('/data/dist_dict.json').then(function(result) {
+    $scope.distances = result.data;
+  });
   $http.get('/data/first-debate.json').then(function(results) {
     $scope.debate = results.data.debates;
     angular.forEach(results.data.votes, function(data, key) {
       $scope.votes.push(new vote(key, data));
+    });
+    angular.forEach(results.data.authors, function(data, key) {
+      var authorObj = new author(key, data);
+      $scope.authorsLookup[key] = authorObj;
+      $scope.authors.push(authorObj);
     });
     angular.forEach(results.data.articles, function(data, key) {
       $scope.articles.push(new article(key, data));
@@ -25,10 +46,6 @@ angular.module('causa').controller('ContextController', ['thesis', 'article', 'v
       $scope.theses.push(new thesis(key, data, articles[0], votes));
     });
     $scope.currentArticle = $scope.articles[9];
+    $scope.theses = sortByArticleDate($scope.theses);
   });
-  $scope.sortByArticleDate = function(results) {
-    return results.sort(function(a,b) {
-      return a.article.date - b.article.date;
-    });
-  };
 }]);
