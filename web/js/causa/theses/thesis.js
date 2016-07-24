61b0113ae5a0bfffaaff1d6c7e60e447fa825b2a
milafrerichs <mila.frerichs@gmail.com>
2016-07-24 14:54:50 +0200

    Add author_id to thesis and add more helper methods

--- web/js/causa/theses/thesis.js
+++ web/js/causa/theses/thesis.js
@@ -1,21 +1,34 @@
-angular.module('causa').factory('thesis', function() {
+angular.module('causa').factory('thesis', ['author', function(author) {
   function Theses(key, json, article, votes) {
     this.id = key;
     this.text = json.text;
+    this.author_id = json.created_by;
     this.article = article;
     this.votes = votes;
     this.getVoteCount = function() {
       return this.votes.length;
     };
     this.getPositiveVoteCount = function() {
-      return this.votes.filter(function(vote) { return vote.vote == "1";}).length;
+      return this.getPositiveVotes().length;
     };
     this.getNegativeVoteCount = function() {
-      return this.votes.filter(function(vote) { return vote.vote == "-1";}).length;
+      return this.getNegativeVotes().length;
     };
     this.getNeutralVoteCount = function() {
-      return this.votes.filter(function(vote) { return vote.vote == "0";}).length;
+      return this.getNeutralVotes().length;
+    };
+    this.getGroupedVotes = function() {
+       return _.group_by(this.votes, 'vote');
+    };
+    this.getNeutralVotes= function() {
+      return this.votes.filter(function(vote) { return vote.vote == "0";});
+    };
+    this.getPositiveVotes = function() {
+      return this.votes.filter(function(vote) { return vote.vote == "1";});
+    };
+    this.getNegativeVotes = function() {
+      return this.votes.filter(function(vote) { return vote.vote == "-1";});
     };
   }
   return Theses;
-});
+}]);
